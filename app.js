16394a49546762c5232aee0bb0eb6c5b1a73a677
Sijan-Giri <girisijan346@gmail.com>
2025-02-20 12:08:34 +0545

    Google passport oauth integrated

--- app.js
+++ app.js
@@ -2,10 +2,35 @@ const express = require("express");
 require("./model/index");
 const app = express();
 require("dotenv").config()
+const passport = require("passport")
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user,cb) {
+    cb(null,user)
+})
+
+passport.deserializeUser(function(obj,cb) {
+    cb(null,obj)
+})
 
 app.get("/",(req,res) => {
     res.send("Iam alive")
 })
+
+let GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+
+passport.use(new GoogleStrategy({
+    clientID : process.env.CLIENT_ID,
+    clientSecret : process.env.CLIENT_SECRET,
+    callbackURL : "http://localhost:3000/auth/google/callback"
+},
+function(accessToken,refreshToken,profile,done) {
+    userProfile = profile
+    return done(null,userProfile)
+}
+))
 
 const PORT = process.env.PORT || 4000
 app.listen(PORT,() => {
